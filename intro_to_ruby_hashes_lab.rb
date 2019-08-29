56782a5ff23330022ab3e563763bf92bf75874e3
Jas Spencer <jas.spencer414@gmail.com>
2019-08-29 16:27:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,12 @@
 def new_hash
-  # return an empty hash
+  
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  person = {
+    name: "Jas",
+    height: "6ft"
+  }
 end
 
 def pioneer
